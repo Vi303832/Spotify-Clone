b3aa2c5822dc524da01d67804ea7820fdc5c28e7
Akif <mehmettanyeriakif3@hotmail.com>
2025-01-26 17:50:31 +0300

    Music Player eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 
 
 function App() {
@@ -10,11 +11,8 @@ function App() {
       <div className='h-screen bg-black'>
         <div className='h-[90%] flex'>
           <Sidebar />
-
-
-
         </div>
-
+        <Player />
 
 
 
